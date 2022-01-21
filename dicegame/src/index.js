a28bd1482b2a9321a74cc3871f3dce7b566599d2
김정호 <cloud0406@naver.com>
2022-01-21 17:26:02 +0900

    refactor : JSX, fragment
    * JSX 문법 적용
    * fragment 사용해보기

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -2,9 +2,13 @@ import ReactDOM from 'react-dom'; //'react-dom'이라는 패키지에서 디폴�
 
 // render 메서드는 첫 번째 argument요소로 html 태그를 만들어서 두 번째 argument 요소에 집어 넣어주는 역할
 
+// 아래와 같이 .js 파일 안에 html 태그를 사용하는 문법을 리액트에서 지원하는 'JSX 문법'(JS와 html 섞어 쓸 수 있는 JS 확장된 문법)이라 함, 하지만 html 문법을 완전히 그대로 사용할 수는 없음 ex)for, class,  또한 두번째 단어부터 대문자를 사용하는 '카멜케이스' 적용해야함 
 //render 메서드로 html code를 만들어줌 (render 메서드는 보통 index.js에서 한번사용됨)
 ReactDOM.render( // 첫번째 argument 요소인 h1태그를 바탕으로 새로운 html 요소를 만들고 두번째 argument값인 'document~('root')값에 넣는 방식으로 동작 
-  <h1>안녕 리액트!</h1>,
+  <>
+    <h1 className='hello'>안녕 리액트!</h1> {/* JS에서 class는 객체지향의 개념으로 사용되기 때문에 html 문법과는 다르게 className을 사용 */}
+    <p>안녕하세요 !</p>
+  </>,
   document.getElementById('root')
 );
 
