d130ffc7d95381f7f9bb92245eefecaf9067751d
김정호 <cloud0406@naver.com>
2022-01-22 14:25:35 +0900

    design : 버튼 추가
    * 구현 앞서 간단한 버튼 추가해보기

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -7,8 +7,10 @@ import { Fragment } from 'react/cjs/react.production.min';
 //render 메서드로 html code를 만들어줌 (render 메서드는 보통 index.js에서 한번사용됨)
 ReactDOM.render(          // 첫번째 argument 요소인 h1태그를 바탕으로 새로운 html 요소를 만들고 두번째 argument값인 'document~('root')값에 넣는 방식으로 동작 
   <>          {/* 리액트에서 JSX문법을 사용하기 위해서는 반드시 하나의 태그로 감싸줘야함(아니면 오류 발생) -> 오류를 막기 위해 <Fragment></Fragment> 사용 -> 간단히 사용하기 위해 <></> 이와 같이 이름 없는 태그로 축약형 문법으로 자주 사용 */}
-    <h1 className='hello'>안녕 리액트!</h1>         {/* JS에서 class는 객체지향의 개념으로 사용되기 때문에 html 문법과는 다르게 className을 사용 */}
-    <p>안녕하세요 !</p>
+    <h1 id="title">가위바위보</h1>   {/* JS에서 class는 객체지향의 개념으로 사용되기 때문에 html 문법과는 다르게 className을 사용 */}
+    <button className="hand">가위</button>
+    <button className="hand">바위</button>
+    <button className="hand">보</button>
   </>,
   document.getElementById('root')
 );
